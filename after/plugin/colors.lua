3d49c3cd26a0f416ef7661e0e42e10e91ce061bc
evancoons22 <96972938+evancoons22@users.noreply.github.com>
2023-06-12 21:03:35 -0700

    initializing config

---
+++ after/plugin/colors.lua
@@ -0,0 +1,22 @@
+
+
+function ColorMyPens(color)
+    color = color or "kanagawa"
+    vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+end
+
+ColorMyPens()
+
+-- Use this when you can figure out a colorscheme for vim. like rose-pine
+ function ColorMyPencils(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+end
+
+ColorMyPencils() 
