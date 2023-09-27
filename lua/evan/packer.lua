64d2bf970c55fbb2911bd3b7c3c7797ab8d99ed0
evancoons22 <96972938+evancoons22@users.noreply.github.com>
2023-09-27 11:36:27 -0700

    ignore

--- lua/evan/packer.lua
+++ lua/evan/packer.lua
@@ -19,7 +19,13 @@ return require('packer').startup(function(use)
   }) 
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use({
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      after = "nvim-treesitter",
+      requires = "nvim-treesitter/nvim-treesitter",
+  })
   use('nvim-treesitter/playground')
+  use('tpope/vim-dadbod')
   use('ThePrimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
