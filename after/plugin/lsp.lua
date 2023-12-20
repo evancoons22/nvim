21e0ed00c11f0baa5dba7dd6ab4b1f64a31f1350
evancoons22 <96972938+evancoons22@users.noreply.github.com>
2023-12-19 20:47:41 -0800

    lsp keybind change

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,7 +17,7 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-b>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-f>'] = cmp.mapping.select_next_item(cmp_select),
-  -- ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
   -- ["<C-Space>"] = cmp.mapping.complete(),
 })
 
