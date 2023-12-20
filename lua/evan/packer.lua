3e4ddbfc41b31287f0e23c25f040cc9e1e3fb214
Evan Coons <evancoons22@gmail.com>
2023-12-19 21:02:44 -0800

    harpoon 2
    rebasing ubuntu changes

--- lua/evan/packer.lua
+++ lua/evan/packer.lua
@@ -25,7 +25,17 @@ return require('packer').startup(function(use)
       requires = "nvim-treesitter/nvim-treesitter",
   })
   use('nvim-treesitter/playground')
+<<<<<<< HEAD
   use('ThePrimeagen/harpoon')
+=======
+  use('tpope/vim-dadbod')
+  use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
+  use {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      requires = { {"nvim-lua/plenary.nvim"} }
+  }
+>>>>>>> d9aeeb7 (harpoon 2)
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
   use {
