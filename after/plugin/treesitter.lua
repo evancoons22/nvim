433a94a200c6c8e023506a14c8519d87f5b0089c
Evan Coons <evancoons22@gmail.com>
2024-02-20 22:57:57 -0800

    treesitter update

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
+require("nvim-treesitter.install").prefer_git = true
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript","rust","python", "c", "lua", "vim", "bash"},
+  ensure_installed = { "javascript", "typescript","rust","python", "c", "lua", "vim", "bash", "go", "html"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
--- lua/evan/packer.lua
+++ lua/evan/packer.lua
@@ -6,7 +6,9 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use {
+  'wbthomason/packer.nvim'
+  }
 
   use "nvim-lua/plenary.nvim" 
 
