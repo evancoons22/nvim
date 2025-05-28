85eb3cbcb40038a6f576d2c0a4bf1845029ebb5d
Evan Coons <evancoons22@gmail.com>
2025-05-28 08:31:48 -0700

    removing markdown formatter from avante

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -140,16 +140,15 @@ require("lazy").setup({
                     },
                 },
             },
-            {
-                -- Make sure to set this up properly if you have lazy=true
-                'MeanderingProgrammer/render-markdown.nvim',
-                opts = {
-                    file_types = { "markdown", "Avante" },
-                },
-                ft = { "markdown", "Avante" },
-            },
+          --  {
+          --      -- Make sure to set this up properly if you have lazy=true
+          --      'MeanderingProgrammer/render-markdown.nvim',
+          --      opts = {
+          --          file_types = { "markdown", "Avante" },
+          --      },
+          --      ft = { "markdown", "Avante" },
+          --  },
         },
-        -- { 'MeanderingProgrammer/render-markdown.nvim', enabled = false }
     }
 
 })
