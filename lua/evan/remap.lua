e6fb31a87f2480c7448040fa3122a8f93d6f8a96
Evan Coons <evancoons22@gmail.com>
2025-10-13 21:40:29 -0700

    removing more

--- lua/evan/remap.lua
+++ lua/evan/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = ' '
--- vim.keymap.set('n', '<leader>df', vim.cmd.Ex)
 vim.keymap.set('n', '<C-m>', vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- highlight to move things around
@@ -31,7 +30,6 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<C-c>", ":w<CR>")
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux new tmux-sessionizer<CR>") -- jump between projects
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz") -- quick fix navigations... these were rebound in ./after
@@ -41,13 +39,6 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/evan/packer.lua<CR>");
- -- vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
 
 -- MY REMAPS
 -- don't really need these anymore with tmux 
